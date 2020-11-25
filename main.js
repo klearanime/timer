fe55822502b472ab76adb1de6b00f2561433c2fa
Crystal K Young <maybiz@hotmail.com>
2020-11-25 16:58:35 -0500

    add

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 // version 0:
 // console.log('\u0007');
+
+// Vesion 1: 
+// setTimeout(function(){
+//     console.log("3 seconds has passed.")
+//     console.log("\u0007")
+// }, 3000)
+
+// Version 2: 
+const getInput = process.argv[2]
+setTimeout(function(){
+    console.log(getInput + ' seconds has passed.')
+    console.log('\u0007')
+}, getInput * 1000)
+
+// Version 3:
+// const getInput = process.argv.slice(2)
+//     setTimeout(function(){ 
+//         console.log(getInput + ' seconds has passed.')
+//         console.log('\u0007')
+//     }, getInput * 1000)
